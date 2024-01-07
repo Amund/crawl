9b905633fad7a64662e2db3677071f9d998326de
Amund <mailamund@gmail.com>
2024-01-07 23:46:15 +0100

    wip

--- src/crawl.js
+++ src/crawl.js
@@ -1,132 +1,53 @@
-import * as cheerio from 'cheerio'
-import * as fs from 'node:fs'
+import { promise as fastq } from 'fastq'
+import { Worker } from 'worker_threads'
 import { URL } from 'node:url'
-import { performance } from 'node:perf_hooks'
-import pretty from 'pretty-ms'
 
-let url
+import os from 'node:os'
+
+// check url argument
+let startUrl
 try {
     if (!process.argv[2]) {
         throw new Error('Must pass a base URL to crawl')
     }
-    url = new URL(process.argv[2])
+    startUrl = new URL(process.argv[2]).toString()
 } catch (err) {
     console.log(err.message)
     process.exit(1)
 }
 
-const timestamp = Date.now()
-const date = new Date(timestamp)
-const y = date.getFullYear()
-const m = (date.getMonth() + 1).toString().padStart(2, '0')
-const d = date.getDate().toString().padStart(2, '0')
-const h = date.getHours().toString().padStart(2, '0')
-const i = date.getMinutes().toString().padStart(2, '0')
-const s = date.getSeconds().toString().padStart(2, '0')
-const filename = url.hostname + '-' + `${y}${m}${d}-${h}${i}${s}.json`
-const timeout = 3000
+// const cpus = os.cpus().length
+const concurrency = process.env.CONCURRENCY || 1
+const queue = fastq(worker, concurrency)
+const urls = {}
+const tasks = []
 
-const report = {
-    url,
-    filename,
-    timestamp,
-    duration: 0,
-    prettyDuration: '',
-    hits: {},
-}
-
-process.setMaxListeners(0)
-await main(url.toString())
-process.exit(0)
+urls[startUrl] = 1
+tasks.push(queue.push(startUrl))
+await Promise.allSettled(tasks)
+console.log(urls)
 
 // Functions
 
-async function check(url, referer = '') {
-    url = url.replace(/#.*/, '')
-    if (report.hits[url]) {
-        if (!report.hits[url].referers.includes(referer))
-            report.hits[url].referers.push(referer)
-    } else {
-        const hit = {
-            isInternal: url.startsWith(report.url),
-            error: '',
-            status: '',
-            redirected: '',
-            duration: 0,
-            prettyDuration: '',
-            headers: {},
-            referers: [],
-        }
-
-        let response
-        performance.mark('fetch-start')
-        try {
-            response = await fetch(url, { timeout })
-        } catch (err) {
-            hit.error = err.code
-        }
-        performance.mark('fetch-end')
-
-        if (response) {
-            hit.status = response.status
-            hit.redirected = response.redirected ? response.url : ''
-            hit.headers = Object.fromEntries(response.headers)
-            hit.duration = performance.measure(
-                'fetch',
-                'fetch-start',
-                'fetch-end',
-            ).duration
-            hit.prettyDuration = pretty(hit.duration, {
-                formatSubMilliseconds: true,
-            })
-            report.hits[url] = hit
-            console.log(hit.status, url)
-
-            if (
-                response.ok &&
-                hit.isInternal &&
-                hit.headers['content-type'] &&
-                hit.headers['content-type'].startsWith('text/html')
-            ) {
-                const $ = cheerio.load(await response.text())
-                const urls = []
-                let base = $('head base').attr('href') || url
-                for (const item of $('[href]')) urls.push($(item).attr('href'))
-                for (const item of $('[src]')) urls.push($(item).attr('src'))
-                for (const item of urls) {
-                    let link
-                    try {
-                        link = new URL(item).toString()
-                    } catch (err) {}
-                    if (!link) {
-                        try {
-                            link = new URL(item, base).toString()
-                        } catch (err) {}
-                    }
-                    if (link) {
-                        await check(link, url)
-                    }
+async function worker(url) {
+    return await new Promise((ok, ko) => {
+        const worker = new Worker('./src/worker/crawl.js')
+        worker.once('message', (hit) => {
+            // save hit to db
+            // add new tasks
+            for (const newUrl of hit.urls) {
+                if (!urls[newUrl]) {
+                    urls[newUrl] = 1
+                    tasks.push(queue.push(newUrl))
                 }
             }
-        }
-    }
-}
-
-async function main(url) {
-    performance.mark('start')
-    await check(url)
-    performance.mark('end')
-
-    report.duration = performance.measure('total', 'start', 'end').duration
-    report.prettyDuration = pretty(report.duration)
-
-    try {
-        const file = `reports/${report.filename}`
-        const content = JSON.stringify(report, null, 4)
-        fs.writeFileSync(file, content)
-    } catch (err) {
-        console.log(err)
-    }
-
-    console.log('')
+            ok()
+        })
+        // worker.once('message', (o) => {
+        //     console.log(o)
+        //     ok()
+        // })
+        const isInternal = url.startsWith(startUrl)
+        worker.postMessage({ url, isInternal })
+    })
 }
---
+++ src/worker/crawl.js
@@ -0,0 +1,103 @@
+import { parentPort } from 'node:worker_threads'
+import { URL } from 'node:url'
+import { performance } from 'node:perf_hooks'
+import * as cheerio from 'cheerio'
+import pretty from 'pretty-ms'
+import formatDate from '../lib/format-date.js'
+// import * as fs from 'node:fs'
+
+const reportsPath = './reports'
+// const filename = url.hostname + '-' + `${y}${m}${d}-${h}${i}${s}.json`
+const timeout = 3000
+
+parentPort.once('message', async (url) => {
+    parentPort.postMessage(await check(url))
+})
+
+// import formatDate from './lib/format-date.js'
+// import sqlite3 from 'sqlite3'
+
+// const timestamp = Date.now()
+// const date = new Date(timestamp)
+// console.log(formatDate(date))
+// const db = new sqlite3.Database('reports/test.sqlite')
+
+// db.serialize(() => {
+//     db.run('CREATE TABLE lorem (info TEXT)')
+
+//     const stmt = db.prepare('INSERT INTO lorem VALUES (?)')
+//     for (let i = 0; i < 10; i++) {
+//         stmt.run('Ipsum ' + i)
+//     }
+//     stmt.finalize()
+
+//     db.each('SELECT rowid AS id, info FROM lorem', (err, row) => {
+//         console.log(row.id + ': ' + row.info)
+//     })
+// })
+
+// db.close()
+async function check({ url, isInternal }) {
+    url = url.replace(/#.*/, '')
+
+    const hit = {
+        isInternal,
+        error: '',
+        status: '',
+        redirected: '',
+        duration: 0,
+        prettyDuration: '',
+        headers: {},
+        urls: [],
+    }
+
+    let response
+    performance.mark('fetch-start')
+    try {
+        response = await fetch(url, { timeout })
+    } catch (err) {
+        hit.error = err.code
+    }
+    performance.mark('fetch-end')
+
+    if (response) {
+        hit.status = response.status
+        hit.redirected = response.redirected ? response.url : ''
+        hit.headers = Object.fromEntries(response.headers)
+        hit.duration = performance.measure(
+            'fetch',
+            'fetch-start',
+            'fetch-end',
+        ).duration
+        hit.prettyDuration = pretty(hit.duration)
+
+        if (
+            response.ok &&
+            hit.isInternal &&
+            hit.headers['content-type'] &&
+            hit.headers['content-type'].startsWith('text/html')
+        ) {
+            const $ = cheerio.load(await response.text())
+            const urls = []
+            let base = $('head base').attr('href') || url
+            for (const item of $('[href]')) urls.push($(item).attr('href'))
+            for (const item of $('[src]')) urls.push($(item).attr('src'))
+            for (const item of urls) {
+                let link
+                try {
+                    link = new URL(item).toString()
+                } catch (err) {}
+                if (!link) {
+                    try {
+                        link = new URL(item, base).toString()
+                    } catch (err) {}
+                }
+                if (link) {
+                    hit.urls.push(link)
+                }
+            }
+        }
+    }
+
+    return hit
+}
